743eed90e69c006a0b301b554f8f86d0a18e8b8e
Yutao Li <lyutao@daint-ln001.cscs.ch>
2025-08-27 23:13:56 +0200

    setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="flex_sim",
+    version="1.0.0",
+    description="FlexSim: A Python Package for simulations of water adsorption within MOFs considering the framework flexibility",
+    author="Yutao Li",
+    author_email="llgend09@gmail.com",
+    packages=find_packages(),
+)
